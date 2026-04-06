31b0d6e8922d1dbf2dd6bdba3519f81140680dbd
yelnurq <114299301+yelnurq@users.noreply.github.com>
2026-04-06 09:31:08 +0500

    sad

---
+++ backend/app/Models/Course.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model {
+    protected $fillable = ['title', 'description'];
+
+    public function modules() {
+        return $this->hasMany(Module::class)->orderBy('order');
+    }
+}
